03a476e8c9458b08756792ab10bd0c9cee039f68
Jasper-Bergshoeff <jasperbergshoeff@hotmail.com>
2022-11-28 11:11:16 +0100

    Service aangepast met wk elk jaar

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,9 @@ import { Injectable } from '@angular/core';
 })
 export class DataService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  GetAllYears(){
+    return this.http.get('https://k0497.azurewebsites.net/api/years/getallyears');
+  }
 }
