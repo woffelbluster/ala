78c0fe4815e66c42c521f99a99c30a311bd8616a
woffelbluster <118520394+woffelbluster@users.noreply.github.com>
2023-01-09 11:46:07 +0100

    getplayers ingezet

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -10,13 +10,16 @@ export class DataService {
   constructor(private route: ActivatedRoute, private http: HttpClient) {}
     
   GetYears(){
-    return this.http.get('https://k0497.azurewebsites.net/api/years/getallyears');
+    return this.http.get('https://k0497.azurewebsites.net/api/years/getallyears&type=json');
   }
   GetYear(year: number){
-    return this.http.get('https://k0497.azurewebsites.net/api/matches/getallmatchesbyyear?year='+year);
+    return this.http.get('https://k0497.azurewebsites.net/api/matches/getallmatchesbyyear?type=json&year='+year);
   }
   GetMatch(matchid: number){
-    return this.http.get('https://k0497.azurewebsites.net/api/matches/GetMatchesById?matchid='+matchid);
+    return this.http.get('https://k0497.azurewebsites.net/api/matches/GetMatchesById?type=json&matchid='+matchid);
+  }
+  GetPlayers(matchid: number){
+    return this.http.get('https://k0497.azurewebsites.net/api/players/getallplayersbymatch?type=json&matchid='+matchid)
   }
 
   
